eeab92533bbfd544d4af97538bb7d31ac4fbf351
khushi_soni <www.khushi.soni@gmail.com>
2021-08-06 01:28:07 +0530

    Added fn header

--- math.py
+++ math.py
@@ -1,22 +1,22 @@
-#add implimentation
+#Add implimentation
 def addition(x,y):
     return x+y
 
-#substract implimentation    
+#Substract implimentation    
 def substract(x,y):
     return x-y       #on master branch
 
-#multiplication implimentation
+#Multiplication implimentation
 def multilication(x,y):
     return x*y         #on bug456 branch
 
-#devide implimentation
+#Devide implimentation
 def devide(x,y):
     if y == 0 :
         return DIVIDE_BY_0_ERROR      #on master branch
     else:
         return x/y
 
-#square implimentation
+#Square implimentation
 def square(x):
     return x*x
